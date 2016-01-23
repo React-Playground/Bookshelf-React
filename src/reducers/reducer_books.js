4631a280ed27f7ce08dc92c84e384d7f189db143
darwin <oleg.umarov@gmail.com>
2016-01-23 14:47:19 -0600

    set state using reducers

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'Javascript: Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'The Dark Tower'},
-    {title: 'Awesome Ruby'}
+    {title: 'Javascript: Good Parts', 'pages': 101},
+    {title: 'Harry Potter', 'pages': 203},
+    {title: 'The Dark Tower', 'pages': 153},
+    {title: 'Awesome Ruby', 'pages': 231}
   ];
 }
